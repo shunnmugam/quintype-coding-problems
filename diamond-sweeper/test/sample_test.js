388ac7e70562db20813d2a708e9ec52321ad42f6
shunnmugam <shunramit@gmail.com>
2018-05-20 21:39:07 +0530

    =remaining dimond count added

--- diamond-sweeper/test/sample_test.js
+++ diamond-sweeper/test/sample_test.js
@@ -8,24 +8,4 @@ describe("assert testing", () => {
 // Dependencies
 
 require('angular-mocks');
-describe('PasswordController', function() {
-  beforeEach(module('app'));
 
-  var $controller, $rootScope;
-
-  beforeEach(inject(function(_$controller_, _$rootScope_){
-    // The injector unwraps the underscores (_) from around the parameter names when matching
-    $controller = _$controller_;
-    $rootScope = _$rootScope_;
-  }));
-
-  describe('$scope.grade', function() {
-    it('sets the strength to "strong" if the password length is >8 chars', function() {
-      var $scope = $rootScope.$new();
-      var controller = $controller('PasswordController', { $scope: $scope });
-      $scope.password = 'longerthaneightchars';
-      $scope.grade();
-      expect($scope.strength).toEqual('strong');
-    });
-  });
-});
